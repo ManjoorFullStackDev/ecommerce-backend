7f8a9310443308d91428de0f3052a463f2e44c3e
ManjoorFullStackDev <manjoorsk.fullstackdev@gmail.com>
2025-01-12 09:36:43 +0530

    added db connection in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const { dbConnect } = require("./utils/db");
 
 app.use(cors({
   origin: ['http://localhost:3000'],
@@ -14,7 +15,9 @@ app.use(cookieParser());
 app.use('/api', require("./routes/authRoutes"));
 app.get("/", (req, res) => res.send("Welcome to Backend"));
 const port = process.env.PORT || 5000;
-
+dbConnect().then(() => {
+  console.log("Connected to DB...");
+})
 app.listen(port, () => console.log(`Server started on port: ${port}`));
 
 
