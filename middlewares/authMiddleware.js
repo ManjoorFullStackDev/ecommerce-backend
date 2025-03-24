078e29445f96dc4789f271b8e3034c3b5a66d3cf
ManjoorFullStackDev <manjoorsk.fullstackdev@gmail.com>
2025-03-24 12:25:25 +0530

    updated authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,28 @@
 const jwt = require("jsonwebtoken");
 
-module.exports.authMiddleware = async (req, res, next) => {
+const roles = {
+  admin: ["post", "put", "delete", "get"],
+  user: ["get", "post"],
+};
+
+module.exports.authMiddleware = (action) => async (req, res, next) => {
   const { access_token } = req.cookies;
+  console.log(`Request:${access_token}`);
   if (!access_token) {
     return res.status(409).json({
       error: "Please login first",
     });
   } else {
     try {
-      const decodeToken = await jwt.verify(
-        access_token,
-        process.env.SECRET_KEY,
+      const decodeToken = await jwt.decode(access_token,
+        process.env.SECRET_KEY
       );
       req.role = decodeToken.role;
       req.id = decodeToken.id;
+      console.log("action", action, req.role);
+      if (action && !roles[req.role].includes(action)) {
+        return res.status(403).json("permission denied ");
+      }
       next();
     } catch (error) {
       return res.status(409).json({
