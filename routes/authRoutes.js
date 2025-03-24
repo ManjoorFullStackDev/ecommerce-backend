a4b9f3b4db7e15635b8d5179b183267ffff56162
ManjoorFullStackDev <manjoorsk.fullstackdev@gmail.com>
2025-03-24 12:26:32 +0530

    updated api router to add authMiddleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,31 @@
 const authControllers = require("../controllers/authControllers");
 const { authMiddleware } = require("../middlewares/authMiddleware");
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/admin-login', authControllers.admin_login)
-router.get('/get-user', authControllers.getUser)
-router.post('/post-user', authControllers.postUser)
-router.get('/get-user-by-id/:id', authControllers.getUserById)
-router.get('/search-location/:location', authControllers.searchLocations)
-router.get('/get-skills', authControllers.getSkills)
-router.get('/get-all-users', authControllers.getAllUsers)
-router.delete('/delete-user/:id', authControllers.deleteUser)
-router.put('/update-user', authControllers.updateUser)
+router.post("/admin-login", authControllers.admin_login);
+router.get("/get-user", authMiddleware("get"), authControllers.getUser);
+router.post("/post-user", authMiddleware("post"), authControllers.postUser);
+router.get(
+  "/get-user-by-id/:id",
+  authMiddleware("get"),
+  authControllers.getUserById,
+);
+router.get(
+  "/search-location/:location",
+  authMiddleware("get"),
+  authControllers.searchLocations,
+);
+router.get("/get-skills", authMiddleware("get"), authControllers.getSkills);
+router.get(
+  "/get-all-users",
+  authMiddleware("get"),
+  authControllers.getAllUsers,
+);
+router.delete(
+  "/delete-user/:id",
+  authMiddleware("delete"),
+  authControllers.deleteUser,
+);
+router.put("/update-user", authMiddleware("put"), authControllers.updateUser);
+router.post("/register-user", authControllers.registerUser);
 module.exports = router;
--- utils/tokenCreate.js
+++ utils/tokenCreate.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 module.exports.createToken = async (data) => {
     return await jwt.sign(data, process.env.SECRET_KEY, {
-    expiresIn: "30d",
+    expiresIn: "1hr",
   });
 };
